0782822c8b9257abad9541850a907371e95c396d
nyftheart <alexandre.kramer@ynov.com>
2024-04-20 14:57:34 +0200

    test pdf modif pour le php

--- export_pdf.php
+++ export_pdf.php
@@ -8,8 +8,12 @@ use Dompdf\Dompdf;
 // Créer une nouvelle instance de Dompdf
 $dompdf = new Dompdf();
 
-// Charger votre HTML dans Dompdf
-$html = file_get_contents('Sous_Section.php');
+// Exécuter Sous_Section.php pour générer le contenu HTML final
+ob_start(); // Démarre la temporisation de sortie
+include 'Sous_Section.php'; // Exécute le fichier PHP et capture sa sortie dans le tampon de sortie
+$html = ob_get_clean(); // Récupère le contenu du tampon de sortie et l'efface
+
+// Charger le contenu HTML dans Dompdf
 $dompdf->loadHtml($html);
 
 // (Optionnel) Définir les options du PDF (par exemple, la taille du papier et l'orientation)
@@ -21,3 +25,4 @@ $dompdf->render();
 // Télécharger le PDF dans le navigateur
 $dompdf->stream("export.pdf", array("Attachment" => false));
 ?>
+
